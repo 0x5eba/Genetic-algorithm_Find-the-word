52fbfd0c3388c4cd8e7764c77067e7fd0858c3ab
Apt-get Root <AptgetRoot@users.noreply.github.com>
2017-01-20 16:38:21 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,18 +4,21 @@
 
 //"N" is the total population
 //"N" must be over a number a few up "M" , otherwise it will uselessly take more time 
-#define N 50
+#define N 60
 //"M" is the length of "Word"
 #define M 30
 //"Word" accept only the alphabetic character ( uppercase and lowercase ), the space and nothing else
 #define Word "MayBe SoMEThiNg MoRe DIffiCUlT"
-//"Mutation" maximum is 100 ( raccomand keep it down of the 10 ) , is in percentage
-#define Mutation 5
+//"Prob_mutation" maximum is 100 ( raccomand keep it down of the 10 ) , is in percentage
+#define Prob_mutation 5
 
 int word_correct = 0;
 int completed = 0;
 clock_t start;
-int Find(char population[N][M], int *a);
+int Random_population(char population[N][M]);
+int Fitness(char population[N][M], int fitness[N]);
+int Crossover(char population[N][M], int sum);
+int Find(char population[N][M], int a);
 
 int main()
 {
@@ -27,38 +30,14 @@ int main()
 
 	std::cout << "\n\tWord to find: " << Word << "\n";
 	std::cout << "\npopulation: " << N << "\n";
-	std::cout << "mutation: " << Mutation << "%\n";
+	std::cout << "Prob_mutation: " << Prob_mutation << "%\n";
 	
-	for (int a = 0; a < N; a++)
-	{
-		for (int b = 0; b < M; b++)
-		{	
-			population[a][b] = (rand() % 58) + 65;
-			if (!isalpha(population[a][b]))
-			{
-				b--;
-				continue;
-			}
-		}
-	}
-
+	Random_population(population);
 	while (true)
 	{
 		//FITNESS
-		for (int a = 0; a < N; a++)
-		{
-			fitness[a] = 0;
-		}
-
-		for (int a = 0; a < N; a++)
-		{
-			for (int b = 0; b < M; b++)
-			{
-				if (population[a][b] == Word[b])
-					fitness[a]++;
-			}
-		}
-
+		Fitness(population, fitness);
+		
 		//SORTING
 		int max_fitness = fitness[0];
 		for (int a = 0; a < N; a++)
@@ -88,57 +67,96 @@ int main()
 
 		if (sum > N / 2)
 			sum = N / 2;
-
 		//CROSSOVER
-		for (int a = 0; a < sum; a++)
-		{
-			int random_mating = rand() % 101;
-			if (random_mating >= 50)
-			{
-				int random_mate = rand() % sum;
-				for (int b = 0; b < M; b++)
-				{
-					//MUTATION
-					int random_mutation = rand() % 101;
-					if (random_mutation <= Mutation)
-					{
-						if (random_mutation < 2)//per lo spazio
-						{
-							population[a][b] = 32;
-							continue;
-						}
-						population[a][b] = (rand() % 58) + 65;
-						if (population[a][b] > 90 || population[a][b] < 97)
-						{
-							b--;
-							continue;
-						}
-						continue;
-					}
-
-					int random = rand() % 101;
-					if (random < 50)
-					{
-						population[sum+a][b] = population[a][b];
-					}
-					else {
-						population[sum+a][b] = population[random_mate][b];
-					}
-				}
-			}
-			Find(population, &a);
-			if (completed == 1) { system("pause"); return 0; }
-		}
+		Crossover(population, sum);
+		if (completed == 1) { system("pause"); return 0; }
 		sum = 0;
 	}
 	return 0;
 }
+int Random_population(char population[N][M])
+{
+	for (int a = 0; a < N; a++)
+	{
+		for (int b = 0; b < M; b++)
+		{
+			population[a][b] = (rand() % 58) + 65;
+			if (!isalpha(population[a][b]))
+			{
+				b--;
+				continue;
+			}
+		}
+	}
+	return population[N][M];
+}
 
-int Find(char population[N][M], int *a)
+int Fitness(char population[N][M], int fitness[N])
+{
+	for (int a = 0; a < N; a++)
+	{
+		fitness[a] = 0;
+	}
+
+	for (int a = 0; a < N; a++)
+	{
+		for (int b = 0; b < M; b++)
+		{
+			if (population[a][b] == Word[b])
+				fitness[a]++;
+		}
+	}
+	return fitness[N];
+}
+
+int Crossover(char population[N][M], int sum)
+{
+	for (int a = 0; a < sum; a++)
+	{
+		int random_mating = rand() % 101;
+		if (random_mating >= 50)
+		{
+			int random_mate = rand() % sum;
+			for (int b = 0; b < M; b++)
+			{
+				//MUTATION
+				int random_Prob_mutation = rand() % 101;
+				if (random_Prob_mutation <= Prob_mutation)
+				{
+					if (random_Prob_mutation < 2)//per lo spazio
+					{
+						population[a][b] = 32;
+						continue;
+					}
+					population[a][b] = (rand() % 58) + 65;
+					if (population[a][b] > 90 || population[a][b] < 97)
+					{
+						b--;
+						continue;
+					}
+					continue;
+				}
+
+				int random = rand() % 101;
+				if (random < 50)
+				{
+					population[sum + a][b] = population[a][b];
+				}
+				else {
+					population[sum + a][b] = population[random_mate][b];
+				}
+			}
+		}
+		Find(population, a);
+	}
+	return population[N][M];
+}
+
+int Find(char population[N][M], int a)
 {
 	for (int x = 0; x < M; x++)
 	{
-		if (population[*a][x] == Word[x])
+		if (population[a][x] == Word[x])
 		{
 			word_correct++;
 		}
@@ -147,7 +165,7 @@ int Find(char population[N][M], int *a)
 			std::cout << "\n\n\tWORD FIND:   ";
 			for (int x = 0; x < M; x++)
 			{
-				std::cout << population[*a][x] << " ";
+				std::cout << population[a][x] << " ";
 			}
 			clock_t end;
 			end = clock();
